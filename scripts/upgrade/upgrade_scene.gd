c286db03dfabcf6433c1c077e9aea963d1f6bc47
Thanushan Satheeskumar <80972937+Thanushan925@users.noreply.github.com>
2026-04-10 11:15:31 -0400

    Modified upgrade scene with UI

--- scripts/upgrade/upgrade_scene.gd
+++ scripts/upgrade/upgrade_scene.gd
@@ -1,10 +1,19 @@
 extends Control
 
-@onready var upgrade_move_button = $CenterContainer/VBoxContainer/UpgradeMove
-@onready var increase_hp_button = $CenterContainer/VBoxContainer/IncreaseHP
+@onready var background = $Background
+
+@onready var upgrade_move_title = $MarginContainer/VBoxContainer/HBoxContainer/UpgradeMoveCard/VBoxContainer/Title
+@onready var upgrade_move_description = $MarginContainer/VBoxContainer/HBoxContainer/UpgradeMoveCard/VBoxContainer/Description
+@onready var upgrade_move_button = $MarginContainer/VBoxContainer/HBoxContainer/UpgradeMoveCard/VBoxContainer/Select
+
+@onready var increase_hp_title = $MarginContainer/VBoxContainer/HBoxContainer/IncreaseHPCard/VBoxContainer/Title
+@onready var increase_hp_description = $MarginContainer/VBoxContainer/HBoxContainer/IncreaseHPCard/VBoxContainer/Description
+@onready var increase_hp_button = $MarginContainer/VBoxContainer/HBoxContainer/IncreaseHPCard/VBoxContainer/Select
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
+	background.texture = load("res://assets/backgrounds/bg1.png")
+	setup_text()
 	upgrade_move_button.pressed.connect(_on_upgrade_move_pressed)
 	increase_hp_button.pressed.connect(_on_increase_hp_pressed)
 
@@ -22,3 +31,10 @@ func _on_increase_hp_pressed():
 		RunManager.build_max_hp_upgrade_message(5, RunManager.max_player_hp)
 	)
 	get_tree().change_scene_to_file("res://scenes/map/map_scene.tscn")
+
+func setup_text():
+	upgrade_move_title.text = "Upgrade a Move"
+	upgrade_move_description.text = "Pick one of your moves for a random bonus:\n+3 damage, -1 cost, or +20% status chance."
+	
+	increase_hp_title.text = "Increase Max HP"
+	increase_hp_description.text = "Gain +5 max HP."
